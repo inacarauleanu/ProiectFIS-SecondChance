e65bcd6a29e321ca6aa4aeb15be2c846fb573df2
ina.carauleanu <ina.carauleanu@gmail.com>
2022-05-07 20:19:59 +0300

    autentificare

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,7 +1,10 @@
 package com.example.demo;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
@@ -10,7 +13,8 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
-
+import javafx.stage.StageStyle;
+import javafx.fxml.FXMLLoader;
 import java.io.File;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -42,43 +46,75 @@ public class HelloController implements Initializable {
         brandingImageView.setImage(brandingImage);
 
     }
-   public void loginButtonOnAction(ActionEvent event)
+   public void signupButtonOnAction(ActionEvent event)
    {
 
-       if(usernameTextField.getText().isBlank()==false && enterPasswordField.getText().isBlank()==false){
-           loginMessageLabel.setText("You try to login");
-           //validateLogin();
-       }else{
-           loginMessageLabel.setText("Please enter username and password");
-       }
+       //if(usernameTextField.getText().isBlank()==false && enterPasswordField.getText().isBlank()==false){
+           //loginMessageLabel.setText("You try to login");
+          // validateSignUp();
+       //}else{
+          // loginMessageLabel.setText("Please enter username and password!");
+           createAccountForm();
+       //}
    }
+    public void loginButtonOnAction(ActionEvent event)
+    {
+
+        if(usernameTextField.getText().isBlank()==false && enterPasswordField.getText().isBlank()==false){
+            loginMessageLabel.setText("You try to login");
+            validateLogin();
+        }else{
+            loginMessageLabel.setText("Please enter username and password!");
+        }
+    }
    public void CancelButtonOnAction(ActionEvent event)
    {
        Stage stage = (Stage) cancelButton.getScene().getWindow();
        stage.close();
     }
 
-   /* public void validateLogin()
+    public void validateLogin()
     {
         DatabaseConnection connectNow = new DatabaseConnection();
         Connection connectDB = connectNow.getConnection();
-        String verifyLogin = "SELECT * FROM user_account WHERE username = '" + usernameTextField.getText() + "AND password='" + enterPasswordField.getText() + "'";
+        String verifyLogin = "SELECT count(1) FROM account_user WHERE username = '" + usernameTextField.getText() + "'" + " AND password ='" + enterPasswordField.getText() +"'";
         try{
             Statement statement = connectDB.createStatement();
             ResultSet queryResult = statement.executeQuery(verifyLogin);
             while(queryResult.next()){
                 if (queryResult.getInt(1)==1) {
                     loginMessageLabel.setText("Congrats!");
+                    //  createAccountForm();
                 }else{
-                    loginMessageLabel.setText("Invalid login. Try again!");
+                    loginMessageLabel.setText("This account does not not exist! Please sign up if you want to create one!");
+                    //createAccountForm();
+
                 }
             }
         }
-            catch(Exception e){
-                e.printStackTrace();
-                e.getCause();
-            }
+        catch(Exception e){
+            e.printStackTrace();
+            e.getCause();
+        }
 
-    }*/
+
+    }
+
+    public void createAccountForm()
+    {
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("register.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 524, 564);
+            Stage registerStage = new Stage();
+            registerStage.initStyle(StageStyle.UNDECORATED);
+            registerStage.setScene(scene);
+            registerStage.show();
+
+        }catch(Exception e){
+            e.printStackTrace();
+            e.getCause();
+        }
+    }
+
 
 }
---
+++ src/main/java/com/example/demo/RegistrationController.java
@@ -0,0 +1,120 @@
+package com.example.demo;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+
+public class RegistrationController implements Initializable {
+    @FXML
+    private Button cancelButton;
+    @FXML
+    private ImageView iconImageView;
+    @FXML
+    private Label registrationMessageLabel;
+    @FXML
+    private PasswordField setPasswordField;
+    @FXML
+    private PasswordField confirmPasswordField;
+    @FXML
+    private Label confirmPasswordLabel;
+    @FXML
+    private TextField firstnameTextField;
+    @FXML
+    private TextField lastnameTextField;
+    @FXML
+    private TextField emailTextField;
+    @FXML
+    private TextField usernameTextField;
+    @FXML
+    private Label checkUsername;
+    @FXML
+    private Label checkEmail;
+
+
+    public void initialize(URL url, ResourceBundle resourceBundle)
+    {
+        File iconFile = new File("images/4068309-200.png");
+        Image iconImage = new Image(iconFile.toURI().toString());
+        iconImageView.setImage(iconImage);
+
+    }
+    public void CancelButtonOnAction(ActionEvent event)
+    {
+        Stage stage = (Stage) cancelButton.getScene().getWindow();
+        stage.close();
+        Platform.exit();
+    }
+
+    public void registerButtonOnAction(ActionEvent event)
+    {
+        if(firstnameTextField.getText().isBlank()==false && lastnameTextField.getText().isBlank()==false && usernameTextField.getText().isBlank()==false && setPasswordField.getText().isBlank()==false && confirmPasswordField.getText().isBlank()==false && emailTextField.getText().isBlank()==false)
+        {
+            if(setPasswordField.getText().equals(confirmPasswordField.getText())) {
+                registerUser();
+                 confirmPasswordLabel.setText(" ");
+
+            }else{
+                confirmPasswordLabel.setText("Password does not match!");
+            }
+        }else{
+            registrationMessageLabel.setText("Please complete all empty fields!");
+            confirmPasswordLabel.setText(" ");
+        }
+
+    }
+
+    public void registerUser() {
+        DatabaseConnection connection = new DatabaseConnection();
+        Connection connectionDB = connection.getConnection();
+
+        String firstname = firstnameTextField.getText();
+        String lastname = lastnameTextField.getText();
+        String username = usernameTextField.getText();
+        String password = setPasswordField.getText();
+        String email = emailTextField.getText();
+
+        String insertFields = "INSERT INTO account_user (lastname, firstname, username, password, email) VALUES('";
+        String insertValues = firstname + "','" + lastname + "','" + username + "','" + password + "','" + email + "')";
+        String insertToRegister = insertFields + insertValues;
+        String verifyUsername = "SELECT count(1) FROM account_user WHERE username = '" + usernameTextField.getText() + "'";
+        String verifyEmail = "SELECT count(1) FROM account_user WHERE email = '" + emailTextField.getText() + "'";
+        try {
+            Statement statement = connectionDB.createStatement();
+            Statement statement1 = connectionDB.createStatement();
+            //Statement statement2 = connectionDB.createStatement();
+            ResultSet queryResult = statement.executeQuery(verifyUsername);
+            //ResultSet queryResult1 = statement.executeQuery(verifyEmail);
+            while (queryResult.next()) {
+                if (queryResult.getInt(1) == 1) {
+                    checkUsername.setText("This username already exists!");
+                } else {
+                    statement1.executeUpdate(insertToRegister);
+                    checkUsername.setText("");
+                    //checkEmail.setText("");
+                    registrationMessageLabel.setText("User has been registered successfully!");
+                }
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            e.getCause();
+        }
+
+    }
+}
+
+
