73a6d0d3ecd51a2b77a32841f49b0f7d8fd3c355
DenisaCernaianu <denisa.cernaianu@student.upt.ro>
2022-05-24 18:32:33 +0300

    SEC-44 <Teste>

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,8 +4,12 @@ module com.example.demo {
     requires javafx.graphics;
     requires java.sql;
     requires mysql.connector.java;
+   requires org.testfx;
+    requires  org.testfx.junit;
 
 
     opens com.example.demo to javafx.fxml;
     exports com.example.demo;
+
+   // exports com.sun.javafx.application;
 }
